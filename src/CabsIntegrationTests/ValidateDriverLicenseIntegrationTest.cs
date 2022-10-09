27273e2b9b580f1d746c83a357e57f47f97c23b1
lbalazy <balazy.lukasz@gmail.com>
2022-10-09 14:03:26 +0200

    Added integration tests project

---
+++ src/CabsIntegrationTests/ValidateDriverLicenseIntegrationTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabsIntegrationTests
+{
+    public class ValidateDriverLicenseIntegrationTest
+    {
+
+        public void CannotCreateActiveDriverWithInvalidLicense()
+        {
+
+        }
+
+        public void CanCreateActiveDriverWithValidLicense()
+        {
+
+        }
+
+        public void CanCreateInactiveDriverWithValidLicense()
+        {
+
+        }
+
+        public void CanChangeLicenseForValidOne()
+        {
+
+        }
+
+        public void CannotChangelicenseForInvalidOne()
+        {
+
+        }
+
+        public void CanActivateDriverWithValidLicense()
+        {
+
+        }
+
+        public void CannotActivateDriverWithInvalidLicense()
+        {
+
+        }
+    }
+}
