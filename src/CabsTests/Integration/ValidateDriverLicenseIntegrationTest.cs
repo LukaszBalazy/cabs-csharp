6ada079b33f90654337ea13186973cb0ad763c33
lbalazy <balazy.lukasz@gmail.com>
2022-10-09 14:49:24 +0200

    Added unit tests for license

--- src/CabsTests/Integration/ValidateDriverLicenseIntegrationTest.cs
+++ src/CabsTests/Integration/ValidateDriverLicenseIntegrationTest.cs
@@ -2,9 +2,6 @@
 using LegacyFighter.Cabs.Entity;
 using LegacyFighter.Cabs.Service;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace LegacyFighter.CabsTests.Integration
@@ -33,34 +30,81 @@ namespace LegacyFighter.CabsTests.Integration
             await this.Awaiting(_ => CreateActiveDriverWithLicense("invalidLicense")).Should().ThrowExactlyAsync<ArgumentException>();
         }
 
-        public void CanCreateActiveDriverWithValidLicense()
+        [Test]
+        public async Task CanCreateActiveDriverWithValidLicense()
         {
+            // when
+            var driver = await CreateActiveDriverWithLicense("FARME100165AB5EW");
 
+            // then
+            var loaded = await DriverService.LoadDriver(driver.Id);
+
+            loaded.Should().NotBeNull();
+            loaded.DriverLicense.Should().Be("FARME100165AB5EW");
+            loaded.Status.Should().Be(Driver.Statuses.Active);
         }
 
-        public void CanCreateInactiveDriverWithValidLicense()
+        [Test]
+        public async Task CanCreateInactiveDriverWithValidLicense()
         {
+            // When
+            var driver = await CreateInactiveDriverWithLicense("FARME100165AB5EW");
 
+            // then
+            var loaded = await DriverService.LoadDriver(driver.Id);
+
+            loaded.Should().NotBeNull();
+            loaded.DriverLicense.Should().Be("FARME100165AB5EW");
+            loaded.Status.Should().Be(Driver.Statuses.Inactive);
         }
 
-        public void CanChangeLicenseForValidOne()
+        [Test]
+        public async Task CanChangeLicenseForValidOne()
         {
+            // Given
+            var driver = await CreateActiveDriverWithLicense("FARME100165AB5EW");
 
+            // When
+            await DriverService.ChangeLicenseNumber("FARME100165AB1EW", driver.Id);
+
+            // Then
+            var loaded = await DriverService.LoadDriver(driver.Id);
+            loaded.Should().NotBeNull();
+            loaded.DriverLicense.Should().Be("FARME100165AB1EW");
         }
 
-        public void CannotChangelicenseForInvalidOne()
+        [Test]
+        public async Task CannotChangelicenseForInvalidOne()
         {
+            // Given
+            var driver = await CreateActiveDriverWithLicense("FARME100165AB5EW");
 
+            // Then
+            await this.Awaiting(_ => DriverService.ChangeLicenseNumber("invalid", driver.Id)).Should().ThrowExactlyAsync<ArgumentException>("Illegal new license no = invalid");
         }
 
-        public void CanActivateDriverWithValidLicense()
+        [Test]
+        public async Task CanActivateDriverWithValidLicense()
         {
+            // Given
+            var driver = await CreateInactiveDriverWithLicense("FARME100165AB5EW");
 
+            // When
+            await DriverService.ChangeDriverStatus(driver.Id, Driver.Statuses.Active);
+
+            // Then
+            var loaded = await DriverService.LoadDriver(driver.Id);
+            loaded.Status.Should().Be(Driver.Statuses.Active);
         }
 
-        public void CannotActivateDriverWithInvalidLicense()
+        [Test]
+        public async Task CannotActivateDriverWithInvalidLicense()
         {
+            // Given
+            var driver = await CreateInactiveDriverWithLicense("invalid");
 
+            // Then
+            await this.Awaiting(_ => DriverService.ChangeDriverStatus(driver.Id, Driver.Statuses.Active)).Should().ThrowExactlyAsync<InvalidOperationException>("Status cannot be ACTIVE.Illegal license no = invalid");
         }
 
         private async Task<Driver> CreateActiveDriverWithLicense(string license)
@@ -71,6 +115,17 @@ namespace LegacyFighter.CabsTests.Integration
               "Jan",
               Driver.Types.Regular,
               Driver.Statuses.Active,
+              "photo");
+        }
+
+        private async Task<Driver> CreateInactiveDriverWithLicense(string license)
+        {
+            return await DriverService.CreateDriver(
+              license,
+              "Kowalski",
+              "Jan",
+              Driver.Types.Regular,
+              Driver.Statuses.Inactive,
               "photo");
         }
     }
