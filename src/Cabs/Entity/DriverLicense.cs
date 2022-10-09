10c9d90b2977e630937b3fbc7bbfde5c3c56c702
lbalazy <balazy.lukasz@gmail.com>
2022-10-09 15:10:23 +0200

    Added DriverLicense and unit tests for that

---
+++ src/Cabs/Entity/DriverLicense.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+namespace LegacyFighter.Cabs.Entity
+{
+    public class DriverLicense
+    {
+        private const string DriverLicenseRegex = "^[A-Z9]{5}\\d{6}[A-Z9]{2}\\d[A-Z]{2}$";
+
+        public string License { get; private set; }
+
+        private DriverLicense(string license)
+        {
+            License = license;
+        }
+
+        public static DriverLicense withLicense(string license)
+        {
+            if (license == null || !license.Any() || !Regex.IsMatch(license, DriverLicenseRegex))
+            {
+                throw new InvalidOperationException("Status cannot be ACTIVE. Illegal license no = " + license);
+            }
+
+            return new DriverLicense(license);
+        }
+
+        public static DriverLicense WithoutValidation(string license) => new DriverLicense(license);
+    }
+}
---
+++ src/CabsTests/Unit/DriverLicenseTests.cs
@@ -0,0 +1,40 @@
+﻿using LegacyFighter.Cabs.Entity;
+using System;
+
+namespace LegacyFighter.CabsTests.Unit
+{
+    public class DriverLicenseTests
+    {
+        [Test]
+        public void CannotCreateInvalidLicense()
+        {
+            this.Invoking(_ => DriverLicense.withLicense("invalid"))
+                .Should()
+                .ThrowExactly<InvalidOperationException>("Status cannot be ACTIVE. Illegal license no = invalid");
+
+            this.Invoking(_ => DriverLicense.withLicense(""))
+                .Should()
+                .ThrowExactly<InvalidOperationException>("Status cannot be ACTIVE. Illegal license no = ");
+        }
+
+        [Test]
+        public void CanCreateValidLicense()
+        {
+            // When
+            var driverLicense = DriverLicense.withLicense("FARME100165AB5EW");
+
+            // Then
+            driverLicense.License.Should().Be("FARME100165AB5EW");
+        }
+
+        [Test]
+        public void CanCreateInvalidLicenseExplicitly()
+        {
+            // When
+            var license = DriverLicense.WithoutValidation("invalid");
+
+            // Then
+            license.License.Should().Be("invalid");
+        }
+    }
+}
